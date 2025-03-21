5df84b7c33ddd5d7f7f50233625260eed77b34b6
HaRiSh337D <harishkumarprasada@gmail.com>
2025-03-21 12:51:12 +0530

    Added test files and updated CI/CD pipeline

---
+++ streamlit-calculator/.github/tests/test_calculator.py
@@ -0,0 +1,16 @@
+# tests/test_calculator.py
+
+import pytest
+from app.calculator import *
+
+def test_addition():
+    assert 5 + 3 == 8
+
+def test_subtraction():
+    assert 10 - 4 == 6
+
+def test_multiplication():
+    assert 3 * 7 == 21
+
+def test_division():
+    assert 10 / 2 == 5
